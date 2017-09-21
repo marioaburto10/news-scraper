3585d38ea901428e435ff4cfe66f6e98deba1add
Mario Aburto <marioaburto10@gmail.com>
2017-09-20 23:25:20 -0400

    chnaged scrape function, will now scrape coindesk.com. Scraping works

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -7,46 +7,50 @@ var cheerio = require("cheerio");
 
 // This function will scrape https://www.developer-tech.com/ (cb is the callback)
 var scrape = function(cb) {
-	// Use the request package to take in the body of the page's html
-	request("https://www.developer-tech.com/", function(err, res, body) {
+	 // Use the request package to take in the body of the page's html
+	request("https://www.coindesk.com/", function(err, res, body) {
 		// body is the actual HTML on the page. Load this into cheerio
+		console.log("scraping")
+		  // Saving this to $ creates a virtual HTML page we can minipulate and
+		  // traverse with the same methods as jQuery
+		  var $ = cheerio.load(body);
 
-	    // Saving this to $ creates a virtual HTML page we can minipulate and
-	    // traverse with the same methods as jQuery
-	    var $ = cheerio.load(body);
+		  // An empty array to save article info
+		  var articles = [];
 
-	    // An empty array to save article info
-	    var articles = [];
-
-	    // Find and loop through each article element that has the "latest-container" id
-	    $("#latest-container article").each(function(i, element) {
-	    	// grab the heading text of each article
-	    	var head = $(this).children("h3").text().trim();
-	    	// grab the summary of each article
-	    	var sum = $(this).children(".summary").text().trim();
-	    	// grab the link of each article
-	    	var lnk = $(this).children("h3").attr("href");
+		  // Find and loop through each article element that has the "latest-container" id
+		  $(".article").each(function(i, element) {
+		    // grab the heading text of each article
+		    var head = $(this).children(".post-info").children("h3").text();
+		    // grab the summary of each article
+		    var sum = $(this).children(".post-info").children("p").text();
+		    // grab the link of each article
+		    var lnk = $(this).children(".post-info").children("h3").children("a").attr("href");
 		    // grab the image link of each article
-	    	var img = $(this).children(".thumb").attr("src");
+		    var img = $(this).children(".picture").children("a").children("img").attr("src");
+		    // console.log("this is an article" , img);
+		  // So long as headline and sum aren't empty or undefined, do the following
+		  if (head && sum && lnk && img) {
 
-			// So long as headline and sum aren't empty or undefined, do the following
-			if (head && sum && lnk && img) {
+		    // This section uses regular expressions and the trim function to tidy the headlines and summaries
+		    // Removing extra lines, extra spacing, extra tabs, etc.. to increase to typographical cleanliness.
+		    var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+		    var sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
 
-				// Initialize an object we will push to the articles array
-				var dataToAdd = {
-				  headline: headNeat,
-				  summary: sumNeat,
-				  link: lnk,
-				  image: img
-				};
+		    // Initialize an object we will push to the articles array
+		    var dataToAdd = {
+		      headline: headNeat,
+		      summary: sumNeat,
+		      link: lnk,
+		      image: img
+		    };
 
-				articles.push(dataToAdd);
-			}
+		    articles.push(dataToAdd);
+		  }
 		});
-	    // After loop is complete, send back the array of articles to the callback function
+		// After loop is complete, send back the array of articles to the callback function
     	cb(articles);
 	});
-
 };
 
 
