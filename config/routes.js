32835bd20e40545c1ae43cb28bd4181c9da69f29
Mario Aburto <marioaburto10@gmail.com>
2017-09-26 14:59:18 -0400

    created heroku credentials

--- config/routes.js
+++ config/routes.js
@@ -26,9 +26,15 @@ module.exports = function(router) {
     headlinesController.fetch(function(err, docs) {
 		// If we don't get any articles back, likely because there are no new
 		// unique articles, send this message back to the user
+		console.log(docs);
 		if (!docs || docs.insertedCount === 0) {
 			res.json({
 			  message: "No new articles today. Check back tomorrow!"
+			});
+		}
+		else if (docs.insertedCount == "undefined") {
+			res.json({
+			  message: "Added new articles!"
 			});
 		}
 		// Otherwise send back a count of how many new articles there are
